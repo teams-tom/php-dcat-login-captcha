0fda87865ca4c54ba5345231afcf37270870d732
teams-tom <tomu9bfg@derdk.onmicrosoft.com>
2025-03-29 18:55:05 +0800

    first commit

---
+++ resources/lang/zh_CN/captcha.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'captcha' => '验证码',
+    'enter_captcha' => '请输入验证码',
+    'refresh_captcha' => '点击刷新验证码',
+    'captcha_error' => '验证码错误',
+
+    'setting' => '登陆验证码设置',
+
+    'charset' => '随机因子',
+    'charset_help' => '随机因子需验证码类型为字符有效',
+
+    'codelen' => '验证码长度',
+    'codelen_help' => '验证码长度需验证码类型为字符有效',
+
+    'fontsize' => '字体大小',
+
+    "auto_refresh" => "自动刷新",
+    "auto_refresh_open" => "开启",
+    "auto_refresh_close" => "关闭",
+    'login_failure' => '是否开启登录失败自动刷新',
+
+    'captcha_type' => '验证码类型',
+    'captcha_type_string' => '字符',
+    'captcha_type_math' => '数学公式',
+
+    'captcha_position' => '验证码位置',
+    'captcha_position_help' => '验证码位置不设置为默认。默认位置为[#login-form fieldset.form-label-group:eq(1)]',
+];
